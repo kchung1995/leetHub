ab70e97904e50d385a72b92dd47986be56f92aad
Kuyho Chung <kchung1995@gmail.com>
2022-11-21 17:29:13 +0900

    Time: 482 ms (37.78%), Space: 11 MB (31.26%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<pair<int, int> > numsPair;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            numsPair.push_back({nums[i], i});
+        }
+        
+        sort(numsPair.begin(), numsPair.end());
+        
+        vector<int> result;
+        bool foundAnswer = false;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                int temp = numsPair[i].first + numsPair[j].first;
+                if (temp > target) break;
+                if (temp == target) {
+                    result.push_back(numsPair[i].second);
+                    result.push_back(numsPair[j].second);
+                    break;
+                }
+            }
+            if (foundAnswer) {
+                break;
+            }
+        }
+        
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
