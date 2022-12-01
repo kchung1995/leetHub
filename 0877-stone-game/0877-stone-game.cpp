e2a04c8e3fcbbc00d5fbce9db4691f86899713af
Kuyho Chung <kchung1995@gmail.com>
2022-12-01 19:34:18 +0900

    Time: 4 ms (74.00%), Space: 7.5 MB (67.83%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
         
-        int alice = 0, bob = 0;
+        int determinant = 0;
         
         sort(piles.begin(), piles.end(), greater<int>());
         
         for (int i = 0; i < piles.size(); i+=2) {
-            alice += piles[i];
-            bob += piles[i + 1];
+            determinant += piles[i];
+            determinant -= piles[i + 1];
         }
         
-        if (alice > bob) {
+        if (determinant > 0) {
             return true;
         }
         else {
