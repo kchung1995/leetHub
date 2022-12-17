1f3ddd5acfea25f97f5e8c2803c371b3fcc595be
Kuyho Chung <kchung1995@gmail.com>
2022-12-17 16:06:01 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (39.15%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int steps[46] = {0, };
+        steps[1] = 1;
+        steps[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            steps[i] = steps[i - 1] + steps[i - 2];
+        }
+        
+        return steps[n];
+    }
+};
