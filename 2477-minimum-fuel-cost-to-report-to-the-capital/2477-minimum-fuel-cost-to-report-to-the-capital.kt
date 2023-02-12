8248e5712e07f75a5e7e2bc3723db7d410bdfdcd
Kuyho Chung <kchung1995@gmail.com>
2023-02-12 13:44:07 +0900

    Time: 1091 ms (70.00%), Space: 105.6 MB (100.00%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.kt
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.kt
@@ -1,5 +1,6 @@
 class Solution {
     private var result: Long = 0
+    
     private fun dfs(nodes: Array<MutableList<Int>>, u: Int, prev: Int, seats: Int): Int {
         var passenger = 1
         for (i in nodes[u]) {
@@ -15,6 +16,7 @@ class Solution {
     fun minimumFuelCost(roads: Array<IntArray>, seats: Int): Long {
         val nodes = Array(roads.size + 1) { mutableListOf<Int>() }
         
+        // set up bidirectional nodes
         for (i in roads) {
             val x = i[0]
             val y = i[1]
