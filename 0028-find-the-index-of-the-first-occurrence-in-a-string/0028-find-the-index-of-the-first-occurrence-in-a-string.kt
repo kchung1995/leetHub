37f9381deb320a2e08e5f997265ffd487e3ce0af
Kuyho Chung <kchung1995@gmail.com>
2023-03-03 14:03:49 +0900

    Time: 131 ms (96.25%), Space: 33.7 MB (66.29%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.kt
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.kt
@@ -1,6 +1,7 @@
 class Solution {
     fun strStr(haystack: String, needle: String): Int {
         for (i in 0 until haystack.length - needle.length + 1) {
+            if (haystack[i] != needle[0]) continue
             if (haystack.substring(i, i + needle.length) == needle)
                 return i
         }
