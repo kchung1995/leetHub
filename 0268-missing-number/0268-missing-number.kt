dfd9996eecc45d371b6f47cc30b24b18b50a3df2
Kuyho Chung <kchung1995@gmail.com>
2023-03-06 02:40:46 +0900

    Time: 268 ms (33.52%), Space: 40 MB (31.03%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun missingNumber(nums: IntArray): Int {
+        nums.sort()
+        for (i in 0 until nums.size) {
+            if (i != nums[i]) return i
+        }
+        return nums.size
+    }
+}
