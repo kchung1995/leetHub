eea4b7a06859e2c044523c09fa7d9fd5d92e407d
Kuyho Chung <kchung1995@gmail.com>
2023-03-08 10:42:38 +0900

    Time: 35 ms (98.11%), Space: 18.9 MB (82.50%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    int getRounding(int banana, int k) {
-        if (banana % k == 0) {
-            return banana / k;
-        }
-        return (banana / k) + 1;
-    }
-    
     int minEatingSpeed(vector<int>& piles, int h) {
         int left = 1;
         int right = *max_element(piles.begin(), piles.end());
@@ -16,19 +9,15 @@ public:
             
             int sum = 0;
             for (auto &i : piles) {
-                sum += getRounding(i, k);
+                sum += (int)ceil((double)i / k);
                 if (sum > h) {
                     left = k + 1;
                     continue;
                 }
             }
             
-            if (sum > h) {
-                left = k + 1;
-            }
-            else {
-                right = k;
-            }
+            if (sum > h) left = k + 1;
+            else right = k;
         }
         
         return left;
