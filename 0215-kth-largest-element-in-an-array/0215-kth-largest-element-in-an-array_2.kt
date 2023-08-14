aa587ac18e756e7bc3f5a47a1f532f3cdcc4cda4
Kuyho Chung <kchung1995@gmail.com>
2023-08-14 23:07:09 +0900

    215. Kth Largest Element in an Array

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array_2.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun findKthLargest(nums: IntArray, k: Int): Int {
+        val pq = PriorityQueue<Int>()
+        
+        for (i in nums) {
+            if (pq.size < k) pq.add(i)
+            else if (i >= pq.peek()) {
+                pq.add(i)
+                pq.poll()
+            }
+        }
+        
+        return pq.peek()
+    }
+}
