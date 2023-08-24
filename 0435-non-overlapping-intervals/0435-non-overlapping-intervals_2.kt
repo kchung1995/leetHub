19ecda4715efb981127a89fb2cbef879ee82bd68
Kuyho Chung <kchung1995@gmail.com>
2023-08-24 23:55:30 +0900

    435. Non-overlapping Intervals

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals_2.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun eraseOverlapIntervals(intervals: Array<IntArray>): Int {
+        intervals.sortWith(compareBy( { it[1] } ))
+
+        var result: Int = 0
+        var boundary: Int = Integer.MIN_VALUE
+
+        intervals.forEach {
+            if (it[0] >= boundary) boundary = it[1]
+            else result++
+        }
+
+        return result
+    }
+}
+
