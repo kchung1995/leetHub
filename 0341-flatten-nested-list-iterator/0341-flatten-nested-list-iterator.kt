506ccc3f8d5e5279df0091b45d188b13f589dca4
Kuyho Chung <kchung1995@gmail.com>
2023-10-20 23:26:11 +0900

    341. Flatten Nested List Iterator

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.kt
@@ -0,0 +1,60 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     constructor()
+ *
+ *     // Constructor initializes a single integer.
+ *     constructor(value: Int)
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     fun isInteger(): Boolean
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     fun getInteger(): Int?
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     fun setInteger(value: Int): Unit
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     fun add(ni: NestedInteger): Unit
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     fun getList(): List<NestedInteger>?
+ * }
+ */
+
+class NestedIterator(nestedList: List<NestedInteger>) {
+    
+    private val resultList = mutableListOf<Int>()
+    var current: Int = 0
+    
+    init {
+        flatten(nestedList)
+    }
+
+    fun next(): Int {
+        val before = current
+        current++
+        return resultList[before]
+    }
+    
+    fun hasNext(): Boolean = current < resultList.size
+
+    private fun flatten(currentList: List<NestedInteger>) {
+        for (i in currentList) {
+            if (i.isInteger()) resultList.add(i.getInteger())
+            else flatten(i.getList())
+        }
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * var obj = NestedIterator(nestedList)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
