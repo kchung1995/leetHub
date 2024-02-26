51dd107415b56d06265b75903ef468bcd5899fe0
Kuyho Chung <kchung1995@gmail.com>
2024-02-26 12:03:51 +0900

    100. Same Tree

---
+++ 0100-same-tree/0100-same-tree.kt
@@ -0,0 +1,24 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        return treeTraversal(p, q)
+    }
+
+    private fun treeTraversal(p: TreeNode?, q: TreeNode?): Boolean {
+        if (p == null && q == null) return true
+        if (p == null || q == null) return false
+        if (p.`val` != q.`val`) return false
+        
+        return treeTraversal(p.left, q.left) && treeTraversal(p.right, q.right)
+    }
+}
+
