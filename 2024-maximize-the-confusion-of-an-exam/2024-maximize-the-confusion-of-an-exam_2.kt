0dc6def5e13e8d5d5eaab37a8045912309c04cd1
Kuyho Chung <kchung1995@gmail.com>
2025-04-28 00:24:36 +0900

    2024. Maximize the Confusion of an Exam

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam_2.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun maxConsecutiveAnswers(answerKey: String, k: Int): Int {
+        var result: Int = 0
+        val count = HashMap<Char, Int>()
+
+        for (i in 0 until answerKey.length) {
+            count.put(answerKey[i], count.getOrDefault(answerKey[i], 0) + 1)
+            val minor = minOf(
+                count.getOrDefault('T', 0),
+                count.getOrDefault('F', 0)
+            )
+            if (minor <= k)
+                result++
+            else
+                count.put(answerKey[i - result], count[answerKey[i - result]]!! - 1)
+        }
+
+        return result
+    }
+}
+
